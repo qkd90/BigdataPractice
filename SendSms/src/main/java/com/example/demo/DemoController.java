79cd210d82106161259db4730d86cf6027b3ab04
qiangren <517054760@qq.com>
2021-06-07 23:49:49 +0800

    Update DemoController.java

--- SendSms/src/main/java/com/example/demo/DemoController.java
+++ SendSms/src/main/java/com/example/demo/DemoController.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
